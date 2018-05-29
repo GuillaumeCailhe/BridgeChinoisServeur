e55aa8de1a0511dd6cdf98bc7d975ff1da86e5af
KyleBehan <34709297+KyleBehan@users.noreply.github.com>
2018-05-29 16:32:32 +0200

    Update IAIntermediaire.java
    Les bugs connus ont été corrigés. J'ai utilisé le client textuel afin de tester le code.

--- src/IA/IAIntermediaire.java
+++ src/IA/IAIntermediaire.java
@@ -7,6 +7,7 @@ package IA;
 
 import LibrairieCarte.Carte;
 import LibrairieCarte.SymboleCarte;
+import LibrairieCarte.ValeurCarte;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -26,18 +27,16 @@ public class IAIntermediaire extends IA {
         
     }
 
-    @Override
     public void informerMain(ArrayList<Carte> main) {
         this.main = main;
         System.out.println(main);
     }
 
-    @Override
     public void informerPiles(ArrayList<Carte> piles) {
         this.piles = piles;
+        System.out.println("informerPiles");
     }
 
-    @Override
     public int prochainCoup() {
         ArrayList<Carte> jouable = new ArrayList<Carte>();
         Random r = new Random();
@@ -46,50 +45,74 @@ public class IAIntermediaire extends IA {
                 jouable.add(c);
             }
         }
+        System.out.println("prochain coup jouable");
         
         if(jouable.size() == 0){
+            System.out.println("prochain coup random");
             return r.nextInt(main.size());
         } else {
-        
-            switch (r.nextInt(3)) {
-                case 0:
-                    return main.indexOf(jouable.get(jouable.size()));
-
-                case 1:
-                    if(jouable.size() == 1 || jouable.size() == 2)
+            int random = r.nextInt(3);
+            int temp = 0;
+            if(random == 0)
+            {
+                System.out.println("prochain coup max");
+                return main.indexOf(jouable.get(jouable.size()-1));
+            }
+            
+            else if(random == 1)
+            {
+                if(jouable.size() == 1 || jouable.size() == 2)
                     {
+                        System.out.println("prochain coup middle card of 1 or 2");
                         return main.indexOf(jouable.get(r.nextInt(jouable.size())));
-                    } else{
+                    } 
+                
+                else{
                         if(jouable.size()%2 == 1)
                         {
+                            System.out.println("prochain coup middle card odd");
                             return main.indexOf(jouable.get((jouable.size()/2)+1));
-                        }else{
+                        }
+                        
+                        else{
+                            System.out.println("prochain coup middle card even");
                             return main.indexOf(jouable.get(jouable.size()/2));
                         }
-                    }
-
-                default:
-                    int temp = 0;
+                }
+            }
+                
+            else{
                     for(Carte c: jouable){
-                        if(comparerCartes(c, carteAdversaire) == 1){ //c>carteAdversaire
+                        
+                        if(carteAdversaire.compareTo(c) <0){ //c>carteAdversaire
+                            System.out.println("prochain coup lowest winning card found");
                             break;
                         }
 
                         else{
+                            System.out.println("prochain coup lowest winning card not found");
                             temp++;
-                        }
+                        }                       
                     }
-                return main.indexOf(jouable.get(temp));
-
+                    
+                    if(temp == 0)
+                    {
+                    return main.indexOf(jouable.get(temp));
+                    }
+                    
+                    else{
+                        temp = temp - 1;
+                        return main.indexOf(jouable.get(temp));
+                    }
             }
             
-        }
-               
+        }        
+                                 
     }
-
-    @Override
+               
     public int prochainePioche() {
-        Carte max = piles.get(0);
+        System.out.println("prochaine pioche");
+        Carte max = new Carte(ValeurCarte.DEUX, SymboleCarte.TREFLE);
         for(Carte c : piles){
             if(c != null && max.compareTo(c) < 0){
                 max = c;
@@ -98,19 +121,20 @@ public class IAIntermediaire extends IA {
         return piles.indexOf(max);
     }
 
-    @Override
     public void informerAtout(SymboleCarte atout) {
+        System.out.println("informerAtout");
         this.atout = atout;
     }
 
-    @Override
     public void informerCoupAdversaire(Carte carte) {
+        System.out.println("informerCoupAdversaire");
         couleurAdversaire = carte.getSymbole();
         carteAdversaire = carte;
     }
-
+    
     @Override
     public void informerPiocheAdversaire(Carte pioche, Carte revelee) {
+        System.out.println("informerPiocheAdversaire");
         piles.set(piles.indexOf(pioche), revelee);
     }
     
@@ -121,6 +145,7 @@ public class IAIntermediaire extends IA {
      * @return 1 si la carte passée en paramètre est inférieure, -1 si elle est supérieure
      */
     private int comparerCartes(Carte carte1, Carte carte2){
+        System.out.println("comparerCartes");
         if(carte1.getSymbole() == atout && carte2.getSymbole() != atout){ // règle de l'atout
             return 1;
         } else if (carte2.getSymbole() == atout && carte1.getSymbole() != atout){ // règle de l'atout
@@ -136,4 +161,3 @@ public class IAIntermediaire extends IA {
     
 }
 
-//assigned the opponent's card to carteAdversaireu
